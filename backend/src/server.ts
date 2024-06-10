327804a6227bafe3e682fb79f520ed004952602f
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-10 19:18:53 +0200

    Add Cross-Origin Resource Sharing  support to backend server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./authentication/routes.js";
 import appLogicRoutes from "./expenses/routes.js";
+import cors from "cors";
 
 // load .env file
 dotenv.config();
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Middleware for parsing JSON bodies
 app.use(express.json());
