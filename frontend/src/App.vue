d8ee8f0ad91ab602ac7dbb6613a9273ac9fb2108
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-12 13:36:45 +0200

    remove linux specific file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import '@/colors.css' // Import colors.css
-
+import { onMounted } from 'vue'
+onMounted(() => {
+  document.title = 'Geldi'
+  let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/svg+xml';
+  link.rel = 'shortcut icon';
+  link.href = '/src/assets/geldi.svg';
+  document.getElementsByTagName('head')[0].appendChild(link);
+})
 </script>
 <template>
   <div class="app">
@@ -17,4 +25,6 @@ header {
 .app {
   /* background-color: var(--color3); */
 }
+
+
 </style>
