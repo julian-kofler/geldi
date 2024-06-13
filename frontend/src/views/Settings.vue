5bce836eef95b61a940f61f8753e7194e81629d3
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-13 16:51:38 +0200

    add buttons for settings

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -3,12 +3,14 @@
     <h1 class="title">Einstellungen</h1>
 
     <PrimaryButton @click="logout">Logout</PrimaryButton>
+    <PrimaryButton @click="change_password">Passwort ändern</PrimaryButton>
+    <PrimaryButton @click="impressum">Impressum</PrimaryButton>
   </div>
   <NavigationBar />
 </template>
 
 <script>
-import PrimaryButton from '../components/PrimaryButton.vue'
+import PrimaryButton from '../components/SecondaryButton.vue'
 import NavigationBar from '../components/NavigationBar.vue'
 export default {
   name: 'SettingsScreen',
@@ -21,7 +23,13 @@ export default {
         localStorage.removeItem('jwt');
         localStorage.removeItem('refreshToken');
         this.$router.push('/login');
-    }
+    },
+    change_password() {
+        this.$router.push('/change-password');
+    },
+    impressum() {
+        this.$router.push('/impressum');
+    },
   }
 }
 </script>
@@ -35,7 +43,7 @@ export default {
   text-align: center;
   font-size: 2em;
 }
-.primary-button {
+.secondary-button {
   width: 100%;
 }
 </style>
