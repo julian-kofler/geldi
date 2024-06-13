01c41248bc491e3eff1df0fd3a79ce4abe844faa
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-13 16:52:06 +0200

    Add secondary button component

---
+++ frontend/src/components/SecondaryButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="secondary-button" @click="onClick"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: 'SecondaryButton',
+  props: {
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.secondary-button {
+  box-sizing: border-box; /* Add this line */
+  background-color: white;
+  color: black;
+  border: 2px solid var(--color5);
+  padding: 10px 20px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  border-radius: 5px;
+}
+
+.secondary-button:hover {
+  background-color: var(--color2);
+  color: black;
+}
+</style>
