322128632327c61543393d857fd6b8d79988ee48
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-21 14:36:54 +0200

    display groups

--- frontend_v2/src/modules/groups/groups.vue
+++ frontend_v2/src/modules/groups/groups.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { getBackend } from "@/components/backendHandler";
+import groupCard from "./components/groupCard.vue";
 
+const groups = ref([]);
+
+const fetchGroups = async () => {
+  const response = await getBackend("/groups");
+  if (response.message === "successful") {
+    groups.value = response.result;
+  } else {
+    //TODO: diplay error msg
+  }
+};
+onMounted(() => {
+  fetchGroups();
+});
 </script>
 
 <template>
-<h1>Meine Gruppen</h1>
+  <h1>Meine Gruppen</h1>
+  <div>
+    <router-link
+      v-for="group in groups"
+      :key="group.id"
+      :to="`/groups/${group.id}`"
+      tag="div"
+      class="group-link"
+    >
+      <groupCard
+        :key="group.id"
+        :id="group.id"
+        :name="group.name"
+        :completed="group.completed"
+      />
+    </router-link>
+  </div>
 </template>
