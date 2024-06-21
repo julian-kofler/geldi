152fc0d6a918256e59fe81e2e52e99ac50915e8c
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-21 14:36:20 +0200

    post and get backend handler

--- frontend_v2/src/components/backendHandler.ts
+++ frontend_v2/src/components/backendHandler.ts
@@ -1,6 +1,84 @@
-const backend_url = "http://localhost:5000/api";
+export const backend_url = "http://localhost:5000/api";
 
-export async function signup(email: string, password: string, nickname: string) {
+const refreshjwtToken = async () : Promise<boolean> => {
+  const response = await fetch(backend_url + "/auth/refreshJWT", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      refreshToken: localStorage.getItem("refreshToken"),
+    }),
+  });
+  if (response.ok) {
+    const data = await response.json();
+    localStorage.setItem("jwt", data.jwt);
+    return true;
+  } else {
+    console.error(
+      "Failed to refresh token:",
+      response.status,
+      response.statusText
+    );
+    return false;
+  }
+};
+
+export async function getBackend(url: string): Promise<any> {
+  let response = await fetch(backend_url + url, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    if (data.message != "Token expired") {
+      console.error(
+        "Failed to fetch",
+        response.status,
+        response.statusText
+      );
+      throw new Error(`Failed to fetch, ${response.status}, ${response.statusText}`);
+    }
+    const refreshed = await refreshjwtToken();
+    if (refreshed) {
+      return await getBackend(url);
+    }
+  }
+  return data;
+}
+export async function postBackend(url: string, body: string): Promise<any> {
+  let response = await fetch(backend_url + url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: body,
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    if (data.message != "Token expired") {
+      console.error(
+        "Failed to fetch",
+        response.status,
+        response.statusText
+      );
+      throw new Error(`Failed to fetch, ${response.status}, ${response.statusText}`);
+    }
+    const refreshed = await refreshjwtToken();
+    if (refreshed) {
+      return await postBackend(url, body);
+    }
+  }
+  return data;
+}
+
+export async function signup(
+  email: string,
+  password: string,
+  nickname: string
+) {
   const url = backend_url + "/auth/signup";
   const body = {
     email: email,
@@ -30,7 +108,7 @@ export async function signup(email: string, password: string, nickname: string) 
   }
 }
 export async function signin(email: string, password: string) {
-  const url = backend_url + "/auth/signup";
+  const url = backend_url + "/auth/login";
   const body = {
     email: email,
     password: password,
