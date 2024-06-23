0bd61541795b3f9b0f478c1dd43277a41fc799b4
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-23 18:21:06 +0200

    component for abort an save button

---
+++ frontend_v2/src/components/abort_save_buttons.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="button-abort-save">
+      <button @click="$emit('abort')" class="btn-secondary" type="submit">
+        abbrechen
+      </button>
+      <button @click="$emit('save')" class="btn-primary" type="submit">
+        Speichern
+      </button>
+    </div>
+</template>
