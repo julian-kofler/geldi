d41be145c30968ec2b6b57757d6faca05211bc8d
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2024-06-26 20:40:19 +0200

    backend calc costs

--- backend/src/expenses/expenses.ts
+++ backend/src/expenses/expenses.ts
@@ -1,10 +1,12 @@
 import mysql from "mysql2/promise";
 import { RowDataPacket } from "mysql2/promise";
 
-import { Expense, ExpenseParams } from "./types";
+import { Expense, ExpenseParams, CompensationPayment } from "./types";
 import { logger } from "../middleware/global.js"
 import exp from "constants";
-
+import { GroupManagement } from "../groups/groups.js";
+import { db } from "../database.js";
+const groupMgmt = new GroupManagement(await db);
 export class ExpenseManagement {
   private db: mysql.Connection;
 
@@ -14,10 +16,10 @@ export class ExpenseManagement {
 
   private isValidExpense(expense: ExpenseParams){
     if (!expense || !expense.amount || !expense.title || !expense.payedBy || !expense.groupId || !expense.payedFor || !expense.timestamp){
-        return false;
+      return false;
     }
     else if (expense.amount < 0 || expense.title.length < 1 || expense.payedBy < 1 || expense.groupId < 1 || expense.payedFor.length < 1){
-        return false;
+      return false;
     }
     return true;
   }
@@ -86,7 +88,7 @@ export class ExpenseManagement {
       for (const expense of expenses) {
         const sql2 = "SELECT userID FROM payed_for WHERE expenseID = ?";
         const [payedFor] = await this.db.execute<RowDataPacket[]>(sql2, [expense.id]);
-        
+
         let expenseParams: ExpenseParams = {
           ...expense,
           payedFor: payedFor.map((row) => row.userID),
@@ -113,12 +115,12 @@ export class ExpenseManagement {
 
       const sql2 = "SELECT userID FROM payed_for WHERE expenseID = ?";
       const [payedFor] = await this.db.execute<RowDataPacket[]>(sql2, [expenseID]);
-        
+
       expenseParams = {
         ...expenses[0],
         payedFor: payedFor.map((row) => row.userID),
       };
-      
+
       return { statusCode: 200, result: expenseParams };
     } 
     catch (error) {
@@ -139,6 +141,151 @@ export class ExpenseManagement {
     catch (error) {
       logger.error((error as Error).message);
       return { statusCode: 500, message: "Internal server error" };
+    }
+  }
+  public async total_cost(
+    groupId: number
+  ): Promise<{ statusCode: number; result: number }> {
+    try {
+      if (groupId === undefined) {
+        return { statusCode: 400, result: 0 };
+      }
+      const res = await this.getExpenses(groupId);
+      if (res.statusCode !== 200) {
+        return { statusCode: 500, result: 0 };
+      }
+      const expenses = res.result
+      let total_cost = 0;
+
+      for(const expense of expenses){
+        total_cost += parseInt(expense.amount as unknown as string);
+      }
+
+      return { statusCode: 200, result: total_cost };
+    } catch (error) {
+      logger.error((error as Error).message);
+      return { statusCode: 500, result: 0 };
+    }
+  }
+  public async my_cost(
+    groupId: number,
+    myUserID: number,
+  ): Promise<{ statusCode: number; result: number }> {
+    try {
+      if (groupId === undefined) {
+        return { statusCode: 400, result: 0 };
+      }
+      const res = await this.getExpenses(groupId);
+      if (res.statusCode !== 200) {
+        return { statusCode: 500, result: 0 };
+      }
+      const expenses = res.result
+      let my_cost = 0;
+
+      for(const expense of expenses){
+        if(expense.payedFor.includes(myUserID)){
+          my_cost += parseInt(expense.amount as unknown as string) / expense.payedFor.length;
+        }
+      }
+      return { statusCode: 200, result: my_cost };
+    } catch (error) {
+      logger.error((error as Error).message);
+      return { statusCode: 500, result: 0 };
+    }
+  }
+  public async my_saldo(
+    groupId: number,
+    myUserID: number,
+  ): Promise<{ statusCode: number; result: number }> {
+    try {
+      if (groupId === undefined) {
+        return { statusCode: 400, result: 0 };
+      }
+
+      const res_expenses = await this.getExpenses(groupId);
+      if (res_expenses.statusCode !== 200) {
+        return { statusCode: 500, result: 0 };
+      }
+      const expenses = res_expenses.result
+
+      const res_my_cost = await this.my_cost(groupId, myUserID);
+      if (res_my_cost.statusCode !== 200) {
+        return { statusCode: 500, result: 0 };
+      }
+      const my_cost = res_my_cost.result;
+
+      let cost_spent_by_me = 0;
+      for(const expense of expenses){
+        if(expense.payedBy == myUserID){
+          cost_spent_by_me += parseInt(expense.amount as unknown as string);
+        }
+      }
+      const my_saldo = cost_spent_by_me - my_cost;
+
+      return { statusCode: 200, result: my_saldo };
+    } catch (error) {
+      logger.error((error as Error).message);
+      return { statusCode: 500, result: 0 };
+    }
+  } 
+  public async compensation_payments(
+    groupId: number,
+  ): Promise<{ statusCode: number; result: CompensationPayment[] }> {
+    try {
+      if (groupId === undefined) {
+        return { statusCode: 400, result: [] };
+      }
+      const res_members = await groupMgmt.getGroup(groupId);
+      if (res_members.statusCode !== 200) {
+        return { statusCode: 500, result: [] };
+      }
+      const members: number[] = res_members.result?.members.map((member: any) => member.userId) || [];
+
+      type saldo = {
+        userID: number,
+        saldo: number,
+      };
+      let salden: saldo[] = [];
+      for (const member of members){
+        const res_saldo = await this.my_saldo(groupId, member);
+        if (res_members.statusCode !== 200) {
+          return { statusCode: 500, result: [] };
+        }
+        salden.push({
+          userID: member,
+          saldo: res_saldo.result,
+        });
+      }
+      salden.sort((a, b) => a.saldo - b.saldo);
+
+      let compensation_payments: CompensationPayment[] = [];
+
+      for (let i = 0; i < members.length; i++) {
+        if (salden[i].saldo < 0) { // -> user i muss etwas bezahlen
+            let j = 0;
+            let betrag = salden[i].saldo;
+            while (salden[i].saldo < 0 && j < members.length) {
+                betrag = salden[i].saldo * -1 < salden[j].saldo ? salden[i].saldo * -1 : salden[j].saldo;
+                if (betrag > 0) {
+                  compensation_payments.push(
+                        {
+                            by: i + 1,
+                            to: j + 1,
+                            amount: betrag,
+                        }
+                    );
+                    salden[i].saldo += betrag;
+                    salden[j].saldo -= betrag;
+                }
+                j++;
+            }
+        }
+    }
+
+      return { statusCode: 200, result: compensation_payments };
+    } catch (error) {
+      logger.error((error as Error).message);
+      return { statusCode: 500, result:  []};
     }
   }
 }
